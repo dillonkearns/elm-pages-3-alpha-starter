b451978867790cc4cbe95de4273fa8d0c4087976
Dillon Kearns <dillon@incrementalelm.com>
2022-08-30 16:46:21 -0700

    Fix paths in adapter script.

--- adapter.mjs
+++ adapter.mjs
@@ -133,8 +133,8 @@ async function render(event, context) {
 
   const compiledElmPath = path.join(__dirname, "elm-pages-cli.js");
   const compiledPortsFile = path.join(__dirname, "port-data-source.js");
-  const renderer = require("../../../../generator/src/render");
-  const preRenderHtml = require("../../../../generator/src/pre-render-html");
+  const renderer = require("../../elm-pages/generator/src/render");
+  const preRenderHtml = require("../../elm-pages/generator/src/pre-render-html");
   try {
     const basePath = "/";
     const mode = "build";

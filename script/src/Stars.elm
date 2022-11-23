419d20937c7d2c572ad2ee022f0d9ad9932ff586
Dillon Kearns <dillon@incrementalelm.com>
2022-11-23 10:46:20 -0800

    Add stars script example.

---
+++ script/src/Stars.elm
@@ -0,0 +1,41 @@
+module Stars exposing (run)
+
+import Cli.Option as Option
+import Cli.OptionsParser as OptionsParser
+import Cli.Program as Program
+import DataSource exposing (DataSource)
+import DataSource.Http
+import Json.Decode as Decode
+import Pages.Script as Script exposing (Script)
+
+
+run : Script
+run =
+    Script.withCliOptions program
+        (\{ username, repo } ->
+            DataSource.Http.get
+                ("https://api.github.com/repos/dillonkearns/" ++ repo)
+                (Decode.field "stargazers_count" Decode.int)
+                |> DataSource.andThen
+                    (\stars ->
+                        Script.log (String.fromInt stars)
+                    )
+        )
+
+
+type alias CliOptions =
+    { username : String
+    , repo : String
+    }
+
+
+program : Program.Config CliOptions
+program =
+    Program.config
+        |> Program.add
+            (OptionsParser.build CliOptions
+                |> OptionsParser.with
+                    (Option.optionalKeywordArg "username" |> Option.withDefault "dillonkearns")
+                |> OptionsParser.with
+                    (Option.optionalKeywordArg "repo" |> Option.withDefault "elm-pages")
+            )

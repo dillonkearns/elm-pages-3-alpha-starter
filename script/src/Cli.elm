a798a1bb82c3a13a3909da82cd277beb832b3476
Dillon Kearns <dillon@incrementalelm.com>
2023-01-13 16:44:24 -0800

    Upgrade to latest.

--- script/src/Cli.elm
+++ script/src/Cli.elm
@@ -94,13 +94,13 @@ createFile moduleName =
         --    }
         |> Pages.Generate.buildWithLocalState
             { view =
-                \maybeUrl sharedModel model app ->
+                \{ maybeUrl, sharedModel, model, app } ->
                     Gen.View.make_.view
                         { title = moduleName |> String.join "." |> Elm.string
                         , body = Elm.list [ Gen.Html.text "Here is your generated page!!!" ]
                         }
             , update =
-                \pageUrl sharedModel app msg model ->
+                \{ pageUrl, sharedModel, app, msg, model } ->
                     Elm.Case.custom msg
                         (Elm.Annotation.named [] "Msg")
                         [ Elm.Case.branch0 "NoOp"
@@ -111,13 +111,13 @@ createFile moduleName =
                             )
                         ]
             , init =
-                \pageUrl sharedModel app ->
+                \{ pageUrl, sharedModel, app } ->
                     Elm.tuple (Elm.record [])
                         (Gen.Effect.none
                             |> Elm.withType effectType
                         )
             , subscriptions =
-                \maybePageUrl routeParams path sharedModel model ->
+                \{ maybePageUrl, routeParams, path, sharedModel, model } ->
                     Gen.Platform.Sub.none
             , model =
                 Alias (Elm.Annotation.record [])
